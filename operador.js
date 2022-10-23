fad90f2cc436497ec8cde4695fe7875a5ea1fab1
Ingrid <ingrid.passarino@gmail.com>
2022-10-23 13:50:36 -0300

    primera entrega

---
+++ operador.js
@@ -0,0 +1,71 @@
+//Ciclo
+for (let i = 1; i <= 100; i++) {
+    
+    
+    let ingresarNombre = prompt("Nombre de tu gato");{
+        if (i< 100){
+            break
+        }
+    }
+    
+    alert(" Felicidades sos el usuario  N° "+i+" Nombre: "+ ingresarNombre);
+        
+}
+
+//Declaro variables
+let nombreIngresado = prompt ("Nombre de tu gato")
+    
+let pesoGato = parseInt (prompt ("¿Cual es el peso actual de tu gato en gramos?"));
+    
+let actividadGato = prompt ("¿Cual es el nivel de actividad de tu gato? 1: sedentario/de interior 2: actividad media 3: de exterior");
+    
+let castradoGato = prompt ("¿Esta castrado?");
+    
+let edadGato = prompt ("¿Cuantos años tiene tu gato?");
+    
+let porcentaje1 = 0.03
+let porcentaje2 = 0.035
+let porcentaje3 = 0.04
+
+
+//Resuelvo mediante function con condicionales
+function multiplicar (pesoGato, porcentaje){
+    if (pesoGato >= 7000 && actividadGato == 1 && castradoGato == "si" && edadGato >=1){
+        let resultado = pesoGato * porcentaje
+    return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+    }
+    else if(pesoGato < 7000 && actividadGato == 2 && castradoGato == "si" && edadGato>=1){
+        let resultado = pesoGato * porcentaje
+    return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+    }
+    else if (pesoGato < 6000 && actividadGato == 3 && castradoGato == "si" && edadGato>1){
+        let resultado = pesoGato * porcentaje
+    return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+    }
+
+    
+}
+let vianda = multiplicar (pesoGato, 0.03)
+console.log (vianda);
+
+
+let gato = {
+    nombre: "Benito",
+    
+}
+
+//else if(pesoGato < 7000 && actividadGato == 2 && castradoGato == "si" && edadGato>=1){
+    //alert(pesoGato * 0.035)
+//}
+//else if (pesoGato < 6000 && actividadGato == 3 && castradoGato == "si" && edadGato>1){
+    //alert (pesoGato * 0.04)
+//}
+//else if(pesoGato < 6000 && actividadGato == 3 && castradoGato == "no" && edadGato >1){
+    //alert(pesoGato * 0.045)
+//}
+//else if(edadGato < 1){
+    //alert("Ver tabla gatitos en crecimiento")
+//}
+//else{
+    //alert(pesoGato * 0.035)
+//}
