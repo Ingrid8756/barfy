9d2b2878ea216638c47ad6f036c7cfbf36e9f73f
Ingrid <ingrid.passarino@gmail.com>
2022-11-03 20:26:43 -0300

    segunda entrega

--- operador.js
+++ operador.js
@@ -59,8 +59,49 @@ let vianda = multiplicar ()
 console.log (vianda);
 
 
-let gato = {
-    nombre: "Benito",
-    
+//OBJETOS//
+class Producto {
+    constructor (nombre, cantidad, precio){
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+}
+const producto1 = new Producto ("Dieta completa Pollo para Gato", 1, 1150);
+const producto2 = new Producto ("Dieta completa Vacuna para Gato", 1, 1300);
+const producto3 = new Producto ("Huesos Carnosos", 1, 550);
+const producto4 = new Producto ("Higado Pollo", 0.250, 200);
+const producto5 = new Producto ("Caldo Huesos", 1, 800);
+
+//OPERADOR IN//
+if ("stock" in producto3){
+    console.log (producto3.stock)
+}
+else{
+    console.log ("No disponemos de stock para este producto por el momento")
 }
 
+//OPERADOR FOR.. IN// Recorre todas las propiedades del objeto//
+for (const propiedad in producto4){
+    console.log (propiedad)
+}
+
+
+//ARRAYS
+
+const baseProductos = [producto1, producto2, producto3, producto4, producto5];
+
+baseProductos.forEach ((el) => {
+    console.log (el.precio)
+});
+
+let carrito = [];
+
+let agregarProducto = prompt ("Ingresá el num de producto: 0- Dieta Completa Pollo / 1- Dieta Completa Vacuna / 2- Huesos Carnosos / 3- Higado Pollo / 4- Caldo Huesos")
+
+function comprar (producto){
+    carrito.push (baseProductos [producto]);
+}
+
+comprar (agregarProducto);
+console.log (carrito);
