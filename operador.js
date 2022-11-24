e1273cdc1504e2897820871e86427160c5253193
Ingrid <ingrid.passarino@gmail.com>
2022-11-24 19:10:23 -0300

    tercera entrega

--- operador.js
+++ operador.js
@@ -1,62 +1,62 @@
 //Ciclo
-for (let i = 1; i <= 100; i++) {
+// for (let i = 1; i <= 100; i++) {
     
     
-    let ingresarNombre = prompt("Nombre de tu gato");{
-        if (i< 100){
-            break
-        }
-    }
+//     let ingresarNombre = prompt("Nombre de tu gato");{
+//         if (i< 100){
+//             break
+//         }
+//     }
     
-    alert(" Felicidades sos el usuario  N° "+i+" Nombre: "+ ingresarNombre);
+//     alert(" Felicidades sos el usuario  N° "+i+" Nombre: "+ ingresarNombre);
         
-}
+// }
 
 //Declaro variables
 
-let pesoGato = parseInt (prompt ("¿Cual es el peso actual de tu gato en gramos?"));
+// let pesoGato = parseInt (prompt ("¿Cual es el peso actual de tu gato en gramos?"));
     
-let actividadGato = prompt ("¿Cual es el nivel de actividad de tu gato? 1: sedentario/de interior 2: actividad media 3: de exterior");
+// let actividadGato = prompt ("¿Cual es el nivel de actividad de tu gato? 1: sedentario/de interior 2: actividad media 3: de exterior");
     
-let castradoGato = prompt ("¿Esta castrado?");
+// let castradoGato = prompt ("¿Esta castrado?");
     
-let edadGato = prompt ("¿Cuantos años tiene tu gato?");
+// let edadGato = prompt ("¿Cuantos años tiene tu gato?");
     
-let porcentaje1 = 0.03
-let porcentaje2 = 0.035
-let porcentaje3 = 0.04
-let porcentaje4 = 0.045
+// let porcentaje1 = 0.03
+// let porcentaje2 = 0.035
+// let porcentaje3 = 0.04
+// let porcentaje4 = 0.045
 
-//Resuelvo mediante function con condicionales
-function multiplicar (){
-    if (pesoGato >= 7000 && actividadGato == 1 && castradoGato == "si" && edadGato >=1){
-        let resultado = pesoGato * porcentaje1
-    return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
-    }
-    else if(pesoGato < 7000 && actividadGato == 2 && castradoGato == "si" && edadGato>=1){
-        let resultado = pesoGato * porcentaje2
-    return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
-    }
-    else if (pesoGato < 6000 && actividadGato == 3 && castradoGato == "si" && edadGato>1){
-        let resultado = pesoGato * porcentaje3
-    return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
-    }
-    else if(pesoGato < 6000 && actividadGato == 3 && castradoGato == "no" && edadGato >1){
-        let resultado = pesoGato * porcentaje4
-        return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
-    }
-    else if(edadGato < 1){
-        return `Ver tabla gatitos en crecimiento`;
-    }
-    else{
-        let resultado = pesoGato * porcentaje2
-        return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
-    }
+// //Resuelvo mediante function con condicionales
+// function multiplicar (){
+//     if (pesoGato >= 7000 && actividadGato == 1 && castradoGato == "si" && edadGato >=1){
+//         let resultado = pesoGato * porcentaje1
+//     return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+//     }
+//     else if(pesoGato < 7000 && actividadGato == 2 && castradoGato == "si" && edadGato>=1){
+//         let resultado = pesoGato * porcentaje2
+//     return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+//     }
+//     else if (pesoGato < 6000 && actividadGato == 3 && castradoGato == "si" && edadGato>1){
+//         let resultado = pesoGato * porcentaje3
+//     return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+//     }
+//     else if(pesoGato < 6000 && actividadGato == 3 && castradoGato == "no" && edadGato >1){
+//         let resultado = pesoGato * porcentaje4
+//         return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+//     }
+//     else if(edadGato < 1){
+//         return `Ver tabla gatitos en crecimiento`;
+//     }
+//     else{
+//         let resultado = pesoGato * porcentaje2
+//         return `La cantidad de vianda por dia debe ser ${resultado} gramos`;
+//     }
 
     
-}
-let vianda = multiplicar ()
-console.log (vianda);
+// }
+// let vianda = multiplicar ()
+// console.log (vianda);
 
 
 //OBJETOS//
@@ -67,8 +67,8 @@ class Producto {
         this.precio = precio;
     }
 }
-const producto1 = new Producto ("Dieta completa Pollo para Gato", 1, 1150);
-const producto2 = new Producto ("Dieta completa Vacuna para Gato", 1, 1300);
+const producto1 = new Producto ("Dieta completa Pollo", "1 kg", 1150);
+const producto2 = new Producto ("Dieta completa Vacuna", 1, 1300);
 const producto3 = new Producto ("Huesos Carnosos", 1, 550);
 const producto4 = new Producto ("Higado Pollo", 0.250, 200);
 const producto5 = new Producto ("Caldo Huesos", 1, 800);
@@ -87,21 +87,92 @@ for (const propiedad in producto4){
 }
 
 
-//ARRAYS
+// //ARRAYS
 
 const baseProductos = [producto1, producto2, producto3, producto4, producto5];
 
-baseProductos.forEach ((el) => {
-    console.log (el.precio)
-});
+// baseProductos.forEach ((el) => {
+//     console.log (el.precio)
+// });
 
-let carrito = [];
+let shopContent = document.getElementById ("shopContent");
 
-let agregarProducto = prompt ("Ingresá el num de producto: 0- Dieta Completa Pollo / 1- Dieta Completa Vacuna / 2- Huesos Carnosos / 3- Higado Pollo / 4- Caldo Huesos")
+baseProductos.forEach ((product) =>{
+    let content = document.createElement ("div")
+    content.className = "row row-cols-1 row-cols-md-3 g-4"
 
-function comprar (producto){
-    carrito.push (baseProductos [producto]);
-}
+    shopContent.append (content)
 
-comprar (agregarProducto);
-console.log (carrito);
+    let col = document.createElement ("col")
+    col.className = "col"
+
+    content.append (col)
+
+    let card = document.createElement ("card")
+    card.className = "card"
+    // card.innerHTML = `
+    // <h5>${product.nombre}</h5>
+    // <p>${product.cantidad}</p>
+    // <p>${product.precio}</p>
+    // `;
+    
+    content.append (card)
+
+    let cardBody = document.createElement ("cardBody")
+    cardBody.className = "card-body"
+    cardBody.innerHTML = `
+    <h5>${product.nombre}</h5>
+    <p>${product.cantidad}</p>
+    <p>${product.precio}</p>
+    `;
+
+    content.append (cardBody)
+
+    let agregar = document.createElement ("button")
+    agregar.className = "btn btn-primary"
+    agregar.innerText = "Agregar al Carrito"
+
+    cardBody.append (agregar);
+
+} )  
+// agregar.addEventListener ("click", () => {
+
+// }
+// )
+
+// let boton2 = document.getElementById ("boton2")
+// boton2.addEventListener ("click", llamarboton2)
+
+// function llamarboton2 (){
+//     Swal.fire({
+//           title: 'Agregado!',
+//           text: 'Producto agregado al carrito',
+//           icon: 'success',
+//           confirmButtonText: 'Ok'
+//         })
+
+// }
+
+
+
+// // let carrito = [];
+
+// // let agregarProducto = prompt ("Ingresá el num de producto: 0- Dieta Completa Pollo / 1- Dieta Completa Vacuna / 2- Huesos Carnosos / 3- Higado Pollo / 4- Caldo Huesos")
+
+// // function comprar (producto){
+// //     carrito.push (baseProductos [producto]);
+// // }
+
+// // comprar (agregarProducto);
+// // console.log (carrito);
+
+// //DOM//
+
+// let lista = document.createElement ("ul");
+// lista.classList.add("lista");
+
+// for (producto of baseProductos){
+//     lista.innerHTML += //`<li class = "lista-item"> ${producto}</li>`//
+// }
+// contenido.append(lista);
+
